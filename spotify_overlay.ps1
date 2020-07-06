3e60d35edadcffaf9e8cce415b810cf3bf24df52
ghoulman <thequantum.andrew@gmail.com>
2020-07-05 19:19:18 -0500

    Update spotify_overlay.ps1
    Moved `r to Write-Host
    Decreased number of spaces following Write-Host

--- spotify_overlay.ps1
+++ spotify_overlay.ps1
@@ -8,20 +8,19 @@ While ($True) {
   )
   $no_song = "Not playing..."
   $line = @('\', '|', '/', '-', '\', '|', '/', '-')
-  $write = ""
 
   If ($current_song -eq "Spotify Premium"){
     $no_song | Out-File -FilePath $path -Encoding utf8
-    $write = "`rCurrent song: " + $no_song
+    $write = "Current song: " + $no_song
   } Else {
     $current_song | Out-File -FilePath $path -Encoding utf8
-    $write = "`rCurrent song: " + $current_song
+    $write = "Current song: " + $current_song
   }
 
   $time = 10
   While ($time -gt 0) {
     For ($n = 0; $n -lt $line.length; $n++) {
-      Write-Host -NoNewLine $write $line[$n] "                                   "
+      Write-Host -NoNewLine "`r"$write $line[$n]"                            "
       Start-Sleep -Milliseconds 125
     }
     $time -= 1
